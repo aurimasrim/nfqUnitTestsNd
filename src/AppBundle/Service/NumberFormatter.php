6c25403d854c596c520f77904901a432f7d906a3
Aurimas <aurimas1rimkus@gmail.com>
2017-11-13 19:12:19 +0200

    Create NumberFormatter

---
+++ src/AppBundle/Service/NumberFormatter.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aurimas
+ * Date: 17.11.12
+ * Time: 23.59
+ */
+
+namespace AppBundle\Service;
+
+
+class NumberFormatter
+{
+    public function format(float $numberFormatter)
+    {
+
+    }
+}
---
+++ tests/AppBundle/Service/NumberFormatterTest.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: aurimas
+ * Date: 17.11.12
+ * Time: 23.59
+ */
+
+namespace Tests\AppBundle\Service;
+
+use PHPUnit\Framework\TestCase;
+
+class NumberFormatterTest extends TestCase
+{
+
+}
